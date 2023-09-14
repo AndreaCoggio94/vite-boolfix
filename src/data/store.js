b7d1e919fcfdaf76a33b3371ecfe100afa782c89
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-14 17:18:35 +0200

    added link data in store

--- src/data/store.js
+++ src/data/store.js
@@ -1,3 +1,8 @@
 import { reactive } from "vue";
 
-export const store = reactive({});
+export const store = reactive({
+  searchMovie: "https://api.themoviedb.org/3/search/movie?",
+  apiKey: "api_key=ab7bbb91023578105b166ebbfc1c5d68",
+  query: "&query=",
+  filterData: "",
+});
