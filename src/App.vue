c26c255a34d305d682345d2f9a86880564d46de0
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-15 17:59:49 +0200

    added series filter

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
     AppMain,
   },
   methods: {
-    fetchCards(searchData) {
+    fetchCardsMovies(searchData) {
       axios
         .get("https://api.themoviedb.org/3/search/movie", {
           params: {
@@ -44,15 +44,54 @@ export default {
             };
           });
 
-          this.store.cards = cardsData;
+          this.store.movieCards = cardsData;
         });
     },
-    search() {
-      // console.log("work");
+    fetchCardsSeries(searchData) {
+      axios
+        .get("https://api.themoviedb.org/3/search/tv", {
+          params: {
+            query: searchData,
+            api_key: "ab7bbb91023578105b166ebbfc1c5d68",
+          },
+        })
+        .then((response) => {
+          const cardsData = response.data.results.map((card) => {
+            const {
+              id,
+              original_language,
+              original_name,
+              name,
+              vote_average,
+              overview,
+            } = card;
+            return {
+              id,
+              language: original_language,
+              originalTitle: original_name,
+              title: name,
+              vote: vote_average,
+              desc: overview,
+            };
+          });
 
-      // (this.store.finalLink =
-      //   store.searchMovie + store.apiKey + store.query + store.filterData),
-      this.fetchCards(store.filterData);
+          this.store.seriesCards = cardsData;
+        });
+    },
+    concatCards() {
+      // let movie = this.store.movieCards;
+      // let series = this.store.seriesCards;
+      // this.store.cards = "";
+      this.store.cards = this.store.movieCards.concat(this.store.seriesCards);
+      console.log(this.store.cards);
+    },
+    search() {
+      // (this.store.movieCards = []),
+      //   (this.store.seriesCards = []),
+      //   (this.store.cards = []),
+      this.fetchCardsMovies(store.filterData);
+      this.fetchCardsSeries(store.filterData);
+      this.concatCards();
     },
   },
   created() {},
